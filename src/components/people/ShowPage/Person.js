87f32b4b41ecf23d29d07f886d56674ff2a45193
tfquirk <timothyfquirk@gmail.com>
2019-04-14 16:07:05 +0200

    refactoring to do fetch for individual person

--- src/components/people/ShowPage/Person.js
+++ src/components/people/ShowPage/Person.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // show page for an individual person
 const Person = props => {
-  return (
-    <div className="personShowPage">
-      <div className="personShowPageMain">
-        <div className="personShowImg">
-          <img
-            src="https://dummyimage.com/250x250/fff/aaa"
-            alt="Star Wars Character"
-          />
-        </div>
-        <div className="personShowName">
-          <h1>Name Here</h1>
-        </div>
-        <div className="personShowDetails">
-          <h3>List of details here</h3>
-        </div>
-      </div>
+  let person = null;
 
-      <div className="personShowPageRelatedInfo">
-        <div className="personHomewold">
-          <h3>Homeworld</h3>
+  useEffect(() => {});
+
+  if (!person) {
+    return <h1> Loading...</h1>;
+  } else {
+    return (
+      <div className="personShowPage">
+        <div className="personShowPageMain">
+          <div className="personShowImg">
+            <img
+              src="https://dummyimage.com/250x250/fff/aaa"
+              alt="Star Wars Character"
+            />
+          </div>
+          <div className="personShowName">
+            <h1>Name Here</h1>
+          </div>
+          <div className="personShowDetails">
+            <h3>List of details here</h3>
+          </div>
         </div>
-        <div className="personVehicles">
-          <h3>List vehicles</h3>
+
+        <div className="personShowPageRelatedInfo">
+          <div className="personHomewold">
+            <h3>Homeworld</h3>
+          </div>
+          <div className="personVehicles">
+            <h3>List vehicles</h3>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Person;
