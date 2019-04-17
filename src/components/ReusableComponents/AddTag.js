99931c4828e3134a2969d0894afc6571fcc7b559
tfquirk <timothyfquirk@gmail.com>
2019-04-17 10:50:28 +0200

    additional comments added to AddTag

--- src/components/ReusableComponents/AddTag.js
+++ src/components/ReusableComponents/AddTag.js
@@ -9,25 +9,34 @@ import { ADD_TAG } from "../../types/types";
 // return a create tag input that will perform an dispatch onClick
 // this dispatch is passed to Redux state to be displayed on different components,
 const AddTag = props => {
+  // use hooks for state for tagValue and color of tag
   const [tagValue, setTagValue] = useState(null);
   const [color, setColor] = useState("black");
 
+  // upon user input into input field, update the tagValue (eg tagName)
   const changeTagName = event => {
     setTagValue(event.target.value);
   };
 
+  // upon user selection of color on dropdown, update the color
   const changeColor = event => {
     setColor(event.target.value);
   };
 
+  // check to make sure tag input is not null, then create tag and reset form
   const handleSubmit = event => {
     event.preventDefault();
 
+    // if user has not entered in any information - alert them to do so
     if (tagValue === null) {
       alert("Please enter a tag name to add a tag.");
     } else {
+      // add Tag info to Redux state
       props.addTag(props.item.url, tagValue, color);
+      // empty input field
       document.getElementById("tagName").value = "";
+      // reset value to null, in the case more tags want to be created or user
+      // hits submit a second time
       setTagValue(null);
     }
   };
@@ -58,6 +67,7 @@ const AddTag = props => {
   );
 };
 
+// add a tag to Redux state
 const mapDispatchToProps = dispatch => {
   return {
     addTag: (url, tagName, color) =>
